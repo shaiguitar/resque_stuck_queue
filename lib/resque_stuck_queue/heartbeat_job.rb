e4045606dbaaec49a08ea613f271e7f99df9388d
Shai Rosenfeld <srosenfeld@engineyard.com>
2014-01-31 19:35:12 -0800

    assign the correct redis

--- lib/resque_stuck_queue/heartbeat_job.rb
+++ lib/resque_stuck_queue/heartbeat_job.rb
@@ -6,9 +6,9 @@ module Resque
         attr_accessor :redis
 
         def perform(*args)
+          # TODO rm host, port, namespace hack from enqueue_to and here
           keyname,host,port,namespace,new_time = *args
-          @redis = Redis::Namespace.new(namespace, :redis => Redis.new(:host => host, :port => port))
-          #raise "#{@redis.inspect} must be the same as #{Resque::StuckQueue.redis.inspect}"
+          @redis = Resque::StuckQueue.redis
           @redis.set(keyname, new_time)
           Resque::StuckQueue.logger.info "successfully updated key #{keyname} to #{new_time} at #{Time.now} for #{@redis.inspect}"
         end
