e3ae43c98149ada2373dde564751cf494d7c84aa
Shai Rosenfeld <srosenfeld@engineyard.com>
2014-02-10 10:16:21 -0800

    todo & small refactor

--- test/test_ver_2.rb
+++ test/test_ver_2.rb
@@ -2,20 +2,15 @@
 # $ RESQUE_2=1 bi; RESQUE_2=1 be ruby -I. -Ilib/ test/test_resque_2.rb
 if !ENV['RESQUE_2'].nil?
 
-  require 'minitest'
-  require "minitest/autorun"
-  require "pry"
-  require "logger"
-
-  $:.unshift(".")
-  require "resque_stuck_queue"
-  require File.join(File.expand_path(File.dirname(__FILE__)), "resque", "set_redis_key")
-  require File.join(File.expand_path(File.dirname(__FILE__)), "resque", "refresh_latest_timestamp")
+  require File.join(File.expand_path(File.dirname(__FILE__)), "test_helper")
 
   class TestResque2 < Minitest::Test
 
+    include TestHelper
+
     def setup
       assert (Resque::VERSION.match /^2\./), "must run in 2.0"
+    Resque.redis = Redis.new
     Resque::StuckQueue.config[:redis] = Redis.new
       Redis.new.flushall
     end
@@ -31,6 +26,7 @@ if !ENV['RESQUE_2'].nil?
      Resque::StuckQueue.config[:logger] = Logger.new($stdout)
      Resque::StuckQueue.config[:triggered_handler] = proc { Redis.new.incr("test-incr-key") }
      Resque::StuckQueue.config[:redis] = Redis.new
+     Resque::StuckQueue.config[:queues] = [:app]
 
      #binding.pry
      Resque::StuckQueue.start_in_background
@@ -38,11 +34,9 @@ if !ENV['RESQUE_2'].nil?
      @r2_pid = fork { Resque::StuckQueue.config[:redis] = Redis.new ; Resque::Worker.new("*", :graceful_term => true).work ; Process.waitall }
      sleep 10
 
-     # did not trigger, resque picked up refresh jobs
-     assert_equal Redis.new.get("test-incr-key").to_i, 0
-
-     `kill #{@r2_pid}`
-     Process.waitall
+     # triggers once
+     assert_equal Redis.new.get("test-incr-key").to_i, 1
+     hax_kill_resque
      Resque::StuckQueue.force_stop!
    end
 
